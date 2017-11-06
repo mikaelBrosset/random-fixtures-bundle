3f85d9f8f7e6919fe077478844614f90201afa33
mikaelBrosset <mikael.brosset@gmail.com>
2017-11-06 17:00:26 +0100

    File not found Exception added

---
+++ src/MikaelBrosset/RandomFixturesBundle/Exception/ListNotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Author: Mikael Brosset
+ * Email: mikael.brosset@gmail.com
+ * Date: 06/11/17
+ */
+namespace MikaelBrosset\RandomFixturesBundle\Exception;
+
+use Exception;
+
+class ListNotFoundException extends \Exception
+{
+    public function __construct($class)
+    {
+        parent::__construct(sprintf("list not found for %s", $class));
+    }
+}
