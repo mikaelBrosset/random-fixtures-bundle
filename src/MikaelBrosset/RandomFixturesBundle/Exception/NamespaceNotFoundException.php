3743828ae94a6a7dcc313278c88d0235f16bb611
mikaelBrosset <mikael.brosset@gmail.com>
2017-11-10 16:02:59 +0100

    namespace exception added

---
+++ src/MikaelBrosset/RandomFixturesBundle/Exception/NamespaceNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: Mikael Brosset
+ * Email: mikael.brosset@gmail.com
+ * Date: 06/11/17
+ */
+namespace MikaelBrosset\RandomFixturesBundle\Exception;
+
+class NamespaceNotFoundException extends \Exception
+{
+    public function __construct($class)
+    {
+        parent::__construct(sprintf("class not found for %s, class namespace MUST use PSR-4 standard", $class));
+    }
+}
