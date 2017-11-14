44b8c3b91a9d3bbab71f82f3509d0e9da6c032ea
mikaelBrosset <mikael.brosset@gmail.com>
2017-11-14 13:41:42 +0100

    exception added for missing mandatory parameter

---
+++ src/MikaelBrosset/RandomFixturesBundle/Exception/MissingMandatoryPropertyParameterException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: Mikael Brosset
+ * Email: mikael.brosset@gmail.com
+ * Date: 06/11/17
+ */
+namespace MikaelBrosset\RandomFixturesBundle\Exception;
+
+class MissingMandatoryPropertyParameterException extends \Exception
+{
+    public function __construct($annotationPropertyName, $className, $property)
+    {
+        parent::__construct(sprintf("Mandatory annotation property \"%s\" not found in %s::%s", $annotationPropertyName, $className, $property));
+    }
+}
