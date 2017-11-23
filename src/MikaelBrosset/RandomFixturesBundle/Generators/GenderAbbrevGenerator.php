b5fdb65fde7819c89987ed551049bbbd5cb2b409
mikaelBrosset <mikael.brosset@gmail.com>
2017-11-23 15:54:32 +0100

    Various generators classes added

---
+++ src/MikaelBrosset/RandomFixturesBundle/Generators/GenderAbbrevGenerator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author: Mikael Brosset
+ * Email: mikael.brosset@gmail.com
+ * Date: 07/11/17
+ */
+namespace MikaelBrosset\RandomFixturesBundle\Generators;
+
+use MikaelBrosset\RandomFixturesBundle\Annotation\MBRFProp;
+
+class GenderAbbrevGenerator extends Generator implements GeneratorInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function calculateValue(MBRFProp $MBRFPropFilled)
+    {}
+}
